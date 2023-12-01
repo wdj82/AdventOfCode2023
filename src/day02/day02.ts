cdcb51b4870dbfd4496f1cdc9314ce98f06f3220
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-01 10:01:40 -0600

    day 1 complete

---
+++ src/day02/day02.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 2
+// https://adventofcode.com/2023/day/2
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
