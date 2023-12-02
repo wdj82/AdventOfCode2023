7c08094378fe36a0c609f4babc38256ea82abe88
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-02 11:49:16 -0600

    day 4 setup

---
+++ src/day04/day04.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 4
+// https://adventofcode.com/2023/day/4
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
