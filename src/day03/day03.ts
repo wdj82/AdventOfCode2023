0cc076d85a743c4169fea80a905b0de408f424a1
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-02 11:11:33 -0600

    add day 3 setup

---
+++ src/day03/day03.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 3
+// https://adventofcode.com/2023/day/3
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
