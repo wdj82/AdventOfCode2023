f615ccbd59b0f61c49f30f13d9ef1f4a128732b8
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-04 19:44:53 -0600

    day 6 setup

---
+++ src/day06/day06.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 6
+// https://adventofcode.com/2023/day/6
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
