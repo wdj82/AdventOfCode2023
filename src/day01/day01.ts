15bd393b8fdf4ec85eb93d563eda5cc27754f067
William <wjones@clearc2.com>
2023-12-06 12:19:06 -0600

    clean up

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -28,118 +28,94 @@ const partOne = lines
 
 console.log({ partOne });
 
-function findFirstStringNumberIndex(line: string) {
+function findFirstNumber(line: string) {
   for (let i = 0; i < line.length; i++) {
+    const char = line[i];
+    if (!isNaN(Number(char))) {
+      return char;
+    }
+
     const threeChar = line.slice(i, i + 3);
     if (threeChar === "one") {
-      return { index: i, number: "1" };
+      return "1";
     }
     if (threeChar === "two") {
-      return { index: i, number: "2" };
+      return "2";
     }
     if (threeChar === "six") {
-      return { index: i, number: "6" };
+      return "6";
     }
     const fourChar = line.slice(i, i + 4);
     if (fourChar === "four") {
-      return { index: i, number: "4" };
+      return "4";
     }
     if (fourChar === "five") {
-      return { index: i, number: "5" };
+      return "5";
     }
     if (fourChar === "nine") {
-      return { index: i, number: "9" };
+      return "9";
     }
     const fiveChar = line.slice(i, i + 5);
     if (fiveChar === "three") {
-      return { index: i, number: "3" };
+      return "3";
     }
     if (fiveChar === "seven") {
-      return { index: i, number: "7" };
+      return "7";
     }
     if (fiveChar === "eight") {
-      return { index: i, number: "8" };
+      return "8";
     }
   }
-  return null;
+  throw new Error("no number");
 }
 
-function findLastStringNumberIndex(line: string) {
+function findLastNumber(line: string) {
   for (let i = line.length - 1; i >= 0; i--) {
+    const char = line[i];
+    if (!isNaN(Number(char))) {
+      return char;
+    }
+
     const threeChar = line.slice(i - 2, i + 1);
     if (threeChar === "one") {
-      return { index: i - 2, number: "1" };
+      return "1";
     }
     if (threeChar === "two") {
-      return { index: i - 2, number: "2" };
+      return "2";
     }
     if (threeChar === "six") {
-      return { index: i - 2, number: "6" };
+      return "6";
     }
     const fourChar = line.slice(i - 3, i + 1);
     if (fourChar === "four") {
-      return { index: i - 3, number: "4" };
+      return "4";
     }
     if (fourChar === "five") {
-      return { index: i - 3, number: "5" };
+      return "5";
     }
     if (fourChar === "nine") {
-      return { index: i - 3, number: "9" };
+      return "9";
     }
     const fiveChar = line.slice(i - 4, i + 1);
     if (fiveChar === "three") {
-      return { index: i - 4, number: "3" };
+      return "3";
     }
     if (fiveChar === "seven") {
-      return { index: i - 4, number: "7" };
+      return "7";
     }
     if (fiveChar === "eight") {
-      return { index: i - 4, number: "8" };
+      return "8";
     }
   }
-  return null;
+  throw new Error("no number");
 }
 
 const partTwo = lines
   .map((line) => {
-    let number = "";
+    const firstStringNumber = findFirstNumber(line);
+    const lastStringNumber = findLastNumber(line);
 
-    const firstStringNumber = findFirstStringNumberIndex(line);
-    const lastStringNumber = findLastStringNumberIndex(line);
-
-    let firstNumberIndex = Infinity;
-    let lastNumberIndex = -Infinity;
-    // find first number index
-    for (let i = 0; i < line.length; i++) {
-      const char = line[i];
-      if (!isNaN(Number(char))) {
-        firstNumberIndex = i;
-        break;
-      }
-    }
-
-    // find last number index
-    for (let i = line.length - 1; i >= 0; i--) {
-      const char = line[i];
-      if (!isNaN(Number(char))) {
-        lastNumberIndex = i;
-        break;
-      }
-    }
-
-    if (firstStringNumber && firstStringNumber?.index < firstNumberIndex) {
-      number += firstStringNumber?.number;
-    } else {
-      number += line[firstNumberIndex];
-    }
-
-    if (lastStringNumber && lastStringNumber?.index > lastNumberIndex) {
-      number += lastStringNumber?.number;
-    } else {
-      number += line[lastNumberIndex];
-    }
-
-    return Number(number);
+    return Number(firstStringNumber + lastStringNumber);
   })
   .reduce((acc, curr) => acc + curr, 0);
 
@@ -147,4 +123,3 @@ console.log({ partTwo });
 
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
-
