19addca00df2ee5986730876b220f5df8a412232
William <wjones@clearc2.com>
2023-12-06 09:14:17 -0600

    day 7 setup

---
+++ src/day07/day07.ts
@@ -0,0 +1,7 @@
+// Advent of Code day 7
+// https://adventofcode.com/2023/day/7
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
