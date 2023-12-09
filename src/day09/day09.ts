31cf4215d87e5a3110743e302c236e8fcceaf36f
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-09 09:33:01 -0600

    day 9 complete

--- src/day09/day09.ts
+++ src/day09/day09.ts
@@ -3,5 +3,44 @@
 
 import { rawInput } from "./rawInput";
 
-document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
-document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+const lines = rawInput.split("\n").map((lines) => lines.split(" ").map(Number));
+
+function generateDifferences(numbers: number[]) {
+  const differences: number[] = [];
+  let isAllZeroes = true;
+  for (let i = 0; i < numbers.length - 1; i++) {
+    const first = numbers[i];
+    const second = numbers[i + 1];
+    const difference = second - first;
+    differences.push(difference);
+    if (difference !== 0) {
+      isAllZeroes = false;
+    }
+  }
+  return { differences, isAllZeroes };
+}
+
+function findSequence(numbers: number[], findPrev = false): number {
+  const { differences, isAllZeroes } = generateDifferences(numbers);
+  if (isAllZeroes) {
+    if (findPrev) {
+      return numbers.at(0) ?? 0;
+    }
+    return numbers.at(-1) ?? 0;
+  }
+  const nextNumber = findSequence(differences, findPrev);
+  if (findPrev) {
+    return (numbers.at(0) ?? 0) - nextNumber;
+  }
+  return (numbers.at(-1) ?? 0) + nextNumber;
+}
+
+const partOne = lines.reduce((acc, line) => acc + findSequence(line), 0);
+console.log({ partOne });
+
+const partTwo = lines.reduce((acc, line) => acc + findSequence(line, true), 0);
+console.log({ partTwo });
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
