2b62a79b9aef963b9f3f29976d1a3d7ddc32bc24
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-10 14:48:21 -0600

    day 12 setup

---
+++ src/day12/day12.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 12
+// https://adventofcode.com/2023/day/12
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
