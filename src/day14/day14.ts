bf54a371c2642ebcc43a6c9ba8c391e1f3d150b3
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-12 19:48:03 -0600

    day 14 setup

---
+++ src/day14/day14.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 14
+// https://adventofcode.com/2023/day/14
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
