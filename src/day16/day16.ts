aba24e68dfe463f5e8875e529a8f6d33b2330133
William <wjones@clearc2.com>
2023-12-14 12:36:09 -0600

    day 16 setup

---
+++ src/day16/day16.ts
@@ -0,0 +1,7 @@
+// Advent of Code day 16
+// https://adventofcode.com/2023/day/16
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
