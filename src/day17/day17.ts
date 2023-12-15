31038c6ecf239f275772dfb427b0f6741c7e07ca
William <wjones@clearc2.com>
2023-12-15 16:38:16 -0600

    day 17 setup

---
+++ src/day17/day17.ts
@@ -0,0 +1,7 @@
+// Advent of Code day 17
+// https://adventofcode.com/2023/day/17
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
