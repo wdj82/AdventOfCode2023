0869c85f75220b65e669c95423cc9a283812dc92
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-16 15:16:56 -0600

    day 18 setup

---
+++ src/day18/day18.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 18
+// https://adventofcode.com/2023/day/18
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
