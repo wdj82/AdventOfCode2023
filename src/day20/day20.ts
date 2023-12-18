47943dfb0347b613e18b726b45503730f14ae57e
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-18 09:24:46 -0600

    day 20 setup

---
+++ src/day20/day20.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 20
+// https://adventofcode.com/2023/day/20
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
