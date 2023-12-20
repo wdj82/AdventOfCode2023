7a82451e3445d52e4c407adfc9f4087f4a3e55e1
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-20 15:29:22 -0600

    day 24 setup

---
+++ src/day24/day24.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 24
+// https://adventofcode.com/2023/day/24
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
