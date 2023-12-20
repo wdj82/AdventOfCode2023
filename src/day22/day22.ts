3741daa208462c97947d89f4c72e9589af833025
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-20 15:26:40 -0600

    day 22 setup

---
+++ src/day22/day22.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 22
+// https://adventofcode.com/2023/day/22
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
