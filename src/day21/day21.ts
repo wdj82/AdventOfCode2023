a4f1a2170b7779bebfb520cfcb6fd01b554d51b6
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-20 11:17:10 -0600

    day 21 setup

---
+++ src/day21/day21.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 21
+// https://adventofcode.com/2023/day/21
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
