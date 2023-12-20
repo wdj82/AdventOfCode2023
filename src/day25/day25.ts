0c674c70162ac7e420a5cb747995087efd5b794a
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-20 15:29:59 -0600

    day 25 complete

---
+++ src/day25/day25.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 25
+// https://adventofcode.com/2023/day/25
+
+import { rawInput } from "./rawInput";
+
+document.getElementById("partOne")?.appendChild(document.createTextNode("partOne"));
+document.getElementById("partTwo")?.appendChild(document.createTextNode("partTwo"));
+
