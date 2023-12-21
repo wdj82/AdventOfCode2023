f7a78ce52802ee4cb9227a45eca74aec23c7bc0b
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-21 12:13:04 -0600

    day 21 complete

--- src/utils/getAdjacentCells.ts
+++ src/utils/getAdjacentCells.ts
@@ -1,18 +1,42 @@
 // use for traversing the grid
-const searchDirections = [
-  { x: 0, y: -1 },
-  { x: 0, y: 1 },
-  { x: -1, y: 0 },
-  { x: 1, y: 0 },
+export const searchDirectionsCardinal = [
+  { dx: 0, dy: -1 },
+  { dx: 0, dy: 1 },
+  { dx: -1, dy: 0 },
+  { dx: 1, dy: 0 },
+];
+
+const searchDirectionsOrdinal = [
+  { dx: 0, dy: -1 },
+  { dx: 0, dy: 1 },
+  { dx: -1, dy: 0 },
+  { dx: 1, dy: 0 },
+  { dx: -1, dy: 1 },
+  { dx: 1, dy: 1 },
+  { dx: 1, dy: -1 },
+  { dx: -1, dy: -1 },
 ];
 
 // return in bound adjacent coordinates
-export function getAdjacentCells(currX: number, currY: number, height: number, width: number) {
+export function getAdjacentCellsCardinal(currX: number, currY: number, height: number, width: number) {
   const result = [];
 
-  for (let i = 0; i < searchDirections.length; i++) {
-    const newX = searchDirections[i].x + currX;
-    const newY = searchDirections[i].y + currY;
+  for (let i = 0; i < searchDirectionsCardinal.length; i++) {
+    const newX = searchDirectionsCardinal[i].dx + currX;
+    const newY = searchDirectionsCardinal[i].dy + currY;
+    if (newX >= 0 && newX < height && newY >= 0 && newY < width) {
+      result.push({ newX, newY });
+    }
+  }
+  return result;
+}
+
+export function getAdjacentCellsOrdinal(currX: number, currY: number, height: number, width: number) {
+  const result = [];
+
+  for (let i = 0; i < searchDirectionsOrdinal.length; i++) {
+    const newX = searchDirectionsOrdinal[i].dx + currX;
+    const newY = searchDirectionsOrdinal[i].dy + currY;
     if (newX >= 0 && newX < height && newY >= 0 && newY < width) {
       result.push({ newX, newY });
     }
