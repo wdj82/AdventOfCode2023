d59b62667741753ceff63577e3b475c2ed873539
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-21 08:45:21 -0600

    remove test input

--- src/day05/day05.ts
+++ src/day05/day05.ts
@@ -3,40 +3,6 @@
 
 import { rawInput } from "./rawInput";
 
-const testInput = `seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4`;
-
 const [rawSeeds, ...input] = rawInput.split("\n\n");
 const seeds = rawSeeds.split(": ")[1].split(" ").map(Number);
 
@@ -116,3 +82,4 @@ console.log({ partTwo });
 
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo?.toString() ?? ""));
+
