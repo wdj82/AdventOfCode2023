435aa5d57c3309f728d096f03afe73e7050e9ed2
William Jones <37749088+wdj82@users.noreply.github.com>
2023-12-21 12:13:49 -0600

    clean up

--- src/day21/day21.ts
+++ src/day21/day21.ts
@@ -50,19 +50,6 @@ function findMaxDistance() {
 const partOne = findMaxDistance();
 console.log({ partOne });
 
-// const partTwo = solvePartTwo();
-// console.log({ partTwo });
-
-// p2 idea: since the whole row and line S is on is dots, it must be approximated by quadratic
-// p2 observation: fully filled-out (sub) map will just oscilate between 2 states
-// p2 observation: weird step number! 26501365 = 202300 * map.length + map.length/2
-// reached(steps) grows quadraticly, with len = map.length we need to find reached(offset + i * len), offset is 65 in our case, i is integer
-// we need to get to first 3 i's, then figure out the polynomial
-// reached(steps) in my case as a fnc of i: i=0: 3703, i=1: 32957, i=2: 91379
-// originally plugged this into https://www.dcode.fr/newton-interpolating-polynomial :shrug:
-// wolfram alpha is also a good friend here: https://www.wolframalpha.com/input?i=quadratic+fit+calculator&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3x%22%7D+-%3E%22%7B0%2C+1%2C+2%7D%22&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3y%22%7D+-%3E%22%7B3703%2C+32957%2C+91379%7D%22
-// later figured out we can use day9 and made the solution return actual result
-
 const mod = (n: number, m: number) => ((n % m) + m) % m;
 
 let pos: Record<string, number[]> = {};
